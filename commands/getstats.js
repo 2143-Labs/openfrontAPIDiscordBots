b8206b3bbfb243f20b3adbda9318b293e9978152
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-30 17:01:15 -0400

    Update getstats.js

--- commands/getstats.js
+++ commands/getstats.js
@@ -2,5 +2,7 @@ export default function getstats(msg, args, client) {
   const display = args[2] ?? null;
   const mapName = args[0];
   const statType = args[1];
-  const gameModes = args[3] ?? null
+  const gameModes = args[3] ?? null;
+  let resultMsg = await msg.reply(`📡 Collecting ${statType} from map: \`${mapName}\`...`);
+  const ws = new WebSocket("wss://tktk123456-openfrontio-51.deno.dev/ws");
 }

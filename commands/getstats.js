ac3ca1d3f29b65473c485f52dcccb510ae5d5f77
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-30 16:56:51 -0400

    Create getstats.js

---
+++ commands/getstats.js
@@ -0,0 +1,6 @@
+export default function getstats(msg, args, client) {
+  const display = args[2] ?? null;
+  const mapName = args[0];
+  const statType = args[1];
+  const gameModes = args[3] ?? null
+}

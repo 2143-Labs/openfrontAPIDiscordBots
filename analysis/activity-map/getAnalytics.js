332f0da98cf813e461d1760b3f0a090be4f890c5
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-08-30 19:30:10 -0400

    Updating...

--- analysis/activity-map/getAnalytics.js
+++ analysis/activity-map/getAnalytics.js
@@ -16,6 +16,18 @@ export async function getConstructionEvents(gameId) {
   const events = await res.json()
   return events.events
 }
+export async function getDisplayEvents(gameId) {
+  const res = await fetchEndpoint(gameId, "get_display_events")
+  const events = await res.json()
+  return events.events
+}
+export async function getGeneralEvents(gameId) {
+  const res = await fetchEndpoint(gameId, "get_general_events")
+  const events = await res.json()
+  return events.events
+}
 //console.log(await getPlayers("tj2eiZQE"))
 //console.log(await getPlayerStats("tj2eiZQE"))
-console.log(await getConstructionEvents("tj2eiZQE"))
+//console.log(await getConstructionEvents("tj2eiZQE"))
+//console.log(await getDisplayEvents("tj2eiZQE"))
+//console.log(await getGeneralEvents("tj2eiZQE"))
